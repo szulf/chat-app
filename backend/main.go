c432ebd7a547c42dbbfe582f1b0952233696a1ce
szulf <szymon.mieln@gmail.com>
2024-06-01 19:26:31 +0200

    backend finally! setting cookies on login requests, checking for correct password and username on register, sending username and password on custom post requests on login(dont know if thats good but it works)

--- backend/main.go
+++ backend/main.go
@@ -1,38 +1,78 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
-type requests map[string]func(w http.ResponseWriter, r *http.Request) error
-
-func home(w http.ResponseWriter, r *http.Request) error {
-	w.Write([]byte("this is in the main"))
-	return nil
+type user struct {
+	Username string
+	Password string
 }
 
-func errSite(w http.ResponseWriter) {
-	w.Write([]byte("This doesn't exist!"))
+func setHeaders(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+	w.Header().Add("access-control-expose-headers", "Set-Cookie")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	setHeaders(w)
+
+	if r.Method == "POST" {
+		buffer := make([]byte, 4096)
+		n, err := r.Body.Read(buffer)
+		if err != nil && err != io.EOF {
+			fmt.Println("error reading: ", err)
+			os.Exit(1)
+		}
+		var u user
+		json.Unmarshal(buffer[:n], &u)
+
+		authCookie := http.Cookie{
+			Name:  "test",
+			Value: base64.StdEncoding.EncodeToString([]byte("test")),
+		}
+
+		// Have to set cookie before writing to response
+		http.SetCookie(w, &authCookie)
+		w.Write([]byte("Sent cookie"))
+		w.Write([]byte("\n"))
+
+		w.Write([]byte(u.Username))
+		w.Write([]byte("\n"))
+		w.Write([]byte(u.Password))
+		w.Write([]byte("\n"))
+	}
+
+	w.Write([]byte(r.Method))
+	w.Write([]byte("\n"))
+	w.Write([]byte("login page"))
+	w.Write([]byte("\n"))
+
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	setHeaders(w)
+	w.Write([]byte("register page"))
+}
+
+func chats(w http.ResponseWriter, r *http.Request) {
+	setHeaders(w)
+	w.Write([]byte("chats page"))
 }
 
 func main() {
-	rqs := make(requests)
-	rqs["home"] = home
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
-		if r.RequestURI == "/" {
-			rqs["home"](w, r)
-			return
-		}
+	mux := http.NewServeMux()
 
-		fn, exists := rqs[r.RequestURI[1:]]
-		if !exists {
-			errSite(w)
-			return
-		}
-		fn(w, r)
-	})
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/register", register)
+	mux.HandleFunc("/chats", chats)
 
-	log.Fatal(http.ListenAndServe("localhost:3000", nil))
+	log.Fatal(http.ListenAndServe("localhost:3000", mux))
 }
--- frontend/script.js
+++ frontend/script.js
@@ -1,17 +1,97 @@
-async function idk(path) {
-    fetch(path).then(
-        val => {
-            val.text().then(s => document.querySelector("main").innerHTML = s);
-        },
-    ).catch(
-        err => {
+async function logFetchData(host, path) {
+    fetch(host+path)
+        .then(val => {
+            val.text().then((s) => (console.log(s)));
+        })
+        .catch(err => {
             console.log("Error(1st fetch): ", err);
-        }
-    )
+        });
 }
 
-let request = window.location.href.substring(window.location.href.lastIndexOf("/"));
-// idk("http://localhost:3000" + request);
+const request = window.location.href.substring(window.location.href.lastIndexOf("/"));
 
-//Scroll start at bottom
-document.getElementById("test").scrollTop = document.getElementById("test").scrollHeight;
+// Dont know if thats good
+if (request === "/") {
+    window.location.href = "/login";
+}
+logFetchData("http://localhost:3000", request);
+
+const loginSubmit = document.getElementById("loginSubmit");
+if (loginSubmit !== null) {
+    loginSubmit.addEventListener("click", e => {
+        const username = document.getElementsByName("username")[0].value;
+        const password = document.getElementsByName("password")[0].value;
+
+        fetch("http://localhost:3000/login", {
+            method: "POST",
+            body: JSON.stringify({
+                "username": username,
+                "password": password
+            }),
+            credentials: "include"
+        }).then(val => {
+            val.text().then(s => {
+                console.log(s);
+            });
+        }).catch(err => {
+            console.log("Error(login): ", err);
+        });
+
+        // Gotta check if this will save cookies
+        // window.location.href = "/chats";
+    });
+}
+
+const registerSubmit = document.getElementById("registerSubmit");
+if (registerSubmit !== null) {
+    registerSubmit.addEventListener("click", e => {
+        const username = document.getElementsByName("username")[0].value;
+        const password = document.getElementsByName("password")[0].value;
+        const passwordConfirm = document.getElementsByName("passwordConfirm")[0].value;
+
+        // Move to a different function later
+        // username(4 chars, no spaces)
+        let usernameRgx = /^(?!.*[ ])[a-zA-z\d@$!%*#?&]{4,}/;
+        if (username.match(usernameRgx) === null) {
+            console.log("username must be at least 4 characters long, and cant contain spaces");
+            return;
+        }
+        // password(1 number, 1 lowercase, 1 uppercase, 1 special?, 8 chars)
+        let pswrdRgx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[a-zA-Z\d@$!%*#?&]{8,}/;
+        if (password.match(pswrdRgx) === null) {
+            console.log("password must contain 1 uppercase letter, 1 lowercase letter, 1 digit and 1 special character(@$!%*#?&), and must be at least 8 characters long");
+            return;
+        }
+        // Are passwords the same
+        if (password !== passwordConfirm) {
+            console.log("passwords not the same");
+            return;
+        }
+
+        fetch("http://localhost:3000/login", {
+            method: "POST",
+            body: JSON.stringify({
+                "username": username,
+                "password": password
+            }),
+            credentials: "include"
+        }).then(val => {
+            val.text().then(s => {
+                console.log(s);
+            });
+        }).catch(err => {
+            console.log("Error(login): ", err);
+        });
+
+        // Gotta check if this will save cookies
+        // window.location.href = "/chats";
+    });
+}
+
+// Move to a different script later
+
+// Scroll start at bottom
+const test = document.getElementById("test");
+if (test !== null) {
+    test.scrollTop = document.getElementById("test").scrollHeight;
+}
